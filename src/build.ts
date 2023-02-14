1f05e42e8c382024aa2de4872e4cc26743a6a7ea
williamciva <william.civa@outlook.com>
2023-02-14 17:05:27 -0300

    Configuração Inicial do Projeto

---
+++ src/build.ts
@@ -0,0 +1,9 @@
+const { api } = require('@electron-forge/core');
+
+const main = async () => {
+  await api.package({
+    // add package command options here
+  })
+}
+
+main();
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import path from 'path'
+import { app, BrowserWindow } from 'electron'
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+          },
+    })
+
+    win.loadFile( path.join(__dirname, '../pages/index.html') )
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
