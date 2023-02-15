12047363e1c907e1c8e97930644da8aa73c49d22
williamciva <william.civa@outlook.com>
2023-02-15 17:58:46 -0300

    Modelagem das pastas do produto / criação do projeto ract / automatização dos scripts npm

--- src/build.ts
+++
@@ -1,9 +0,0 @@
-const { api } = require('@electron-forge/core');
-
-const main = async () => {
-  await api.package({
-    // add package command options here
-  })
-}
-
-main();
--- src/electron/index.ts
+++ src/electron/index.ts
@@ -10,7 +10,7 @@ const createWindow = () => {
           },
     })
 
-    win.loadFile( path.join(__dirname, '../pages/index.html') )
+    win.loadURL('http://localhost:3000')
 }
 
 app.whenReady().then(() => {
