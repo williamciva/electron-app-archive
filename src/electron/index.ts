6220eff52a8937a7754eacf86d6a3181e11c21cf
William Civa <williamciva@outlook.com>
2023-02-25 14:38:48 -0300

    Melhoria nos scripts npm e arquiterua do software

--- src/electron/index.ts
+++ src/electron/index.ts
@@ -1,22 +1,32 @@
-import path from 'path'
-import { app, BrowserWindow } from 'electron'
+import path from "path";
+import { app, BrowserWindow } from "electron";
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-          },
-    })
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-    win.loadURL('http://localhost:3000')
-}
+  win.loadURL(
+    process.env.ELECTRON_IS_DEV
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../../build/react/index.html")}`
+  );
+
+  // Open the DevTools.
+  if (process.env.ELECTRON_IS_DEV) {
+    win.webContents.openDevTools({ mode: 'detach' });
+  }
+
+};
 
 app.whenReady().then(() => {
-    createWindow()
-})
+  createWindow();
+});
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-  })
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
